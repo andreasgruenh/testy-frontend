01a286cefe9c3a2ff600505b1e6f54d481a878f1
Andreas Roth <art_github@rtline.de>
2015-08-05 20:24:51 +0200

    Adds alerts for wrong password and backend not available

--- script/route/login/login.js
+++ script/route/login/login.js
@@ -5,6 +5,15 @@ angular.module('testy').controller('loginController',
     serverCommunicator.loginAsync($scope.username, $scope.password).then(
       function(data){
         $state.go('app.home');
+      }, function(data){
+        if(data.status == 401) {
+          $scope.globals.showGlobalAlert('danger', 'Ups!', 'Dein Passwort oder Nutzername war leider falsch');
+        } else if(data.status == 503) {
+          $scope.globals.showGlobalAlert(
+            'danger',
+            'Sorry!',
+            'Das Backend ist leider gerade nicht erreichbar. Melde dich doch bei <a href="mailto:it@paul-consultants.de">it@paul-consultants.de</a>');
+        }
       });
   };
 }]);
