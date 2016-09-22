7d817143a9c053e980d3eee00979ae0c737a62cc
Andreas Roth <art_github@rtline.de>
2016-09-22 23:34:40 +0200

    Removes log statement

--- script/route/questionPool/questionPool.js
+++ script/route/questionPool/questionPool.js
@@ -10,7 +10,7 @@ angular.module('testy').controller('questionPoolController', [
       $scope.subject = questionPool.subject;
       $scope.categories = _.sortBy(categories, 'name');
       $scope.results = _.sortBy(questionPool.results, 'user.accountName');
-      console.log($scope.results);
+
       recalculateHasNoMaterial();
       $scope.edit = function() {
         $scope.editMode = true;
